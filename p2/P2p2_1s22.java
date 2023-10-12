9ed1ed16d94cb5a00e060e9a6a54b05264aacb79
Luis Fernando Mendoza Peña <144190931+LuisilloSavagge@users.noreply.github.com>
2023-10-11 19:44:32 -0600

    Add files via upload

---
+++ p2/P2p2_1s22.java
@@ -0,0 +1,46 @@
+public class P2p2_1S22{
+   public static void main(String[] args) {
+   	int a;
+   	for(a=0;<10;a++){
+   	     System.out.println("Esto se repite"+a);
+    }
+     
+     a=0;
+     for(a<10;a+=2){//a=a+2
+     	 System.out.println("estos son los nuevos valores"+a);
+   	}
+
+    a=1;
+    for (;;a++){
+    	if (a>10) break;
+    	System.out.println("ahora sin 2 elementos " + a);
+
+    } 
+    a=1;
+    for(;;) {
+       System.out.println("Ahora sin parametros");
+       if (a>10) break;
+       a++;
+
+
+       int b=2,c,cp;
+
+       for(a=2;a<101;a++) 
+       	 cp=0;
+       	 for (c=2,c<0A;C++)
+
+    }
+    
+     for (a=2;<101;a++)  {
+    	 for(c=2,c<=a;c++){
+
+    	}	
+    	System.out.println(" " + a);
+    	b++;
+    	if (b==10){
+    		b=1;
+    		System.out.println();
+
+    	}
+    	b++;
+    }
